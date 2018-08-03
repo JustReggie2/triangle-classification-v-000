7291eafc1ac505d9fdaa8f151d87bf38fbe27e9e
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-03 22:48:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
     if trutriangle != self
         raise TriangleError
     else
-      if @length_1 == (@length_2 && @length_3)
+      if @length_1 == @length_2 && @length_2 == @length_3
         :equilateral.inspect
       elsif @length_1 == @length_2 || @length_2 == @length_3 || @length_1 == @length_3
         :isosceles.inspect
