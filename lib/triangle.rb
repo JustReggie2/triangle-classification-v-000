3c95ece2a0dbb944f6319bf956f5f78b80871109
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-03 23:12:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,10 @@ class Triangle
   end
 #binding.pry
   def trutriangle
-    (@length_1 + @length_2 > @length_3 && @length_2 + @length_3 > @length_1 && @length_1 + @length_3 > @length_2)
-     && (@length_1.positive? && @length_2.positive? && @length_3.positive?)
+    if @length_1 + @length_2 > @length_3 
+       @length_2 + @length_3 > @length_1 
+       @length_1 + @length_3 > @length_2
+       @length_1.positive? && @length_2.positive? && @length_3.positive?)
   end
 
   def kind
