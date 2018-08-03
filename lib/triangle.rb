187c2cd15858b1efa336fc563f1987023ccf3e6d
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-03 21:40:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,10 +3,11 @@ class Triangle
   # attr_accessor :length_1, :length_2, :length_3
 
   def inititalize(length_1, length_2, length_3)
+binding.pry
     @length_1 = length_1
     @length_2 = length_2
     @length_3 = length_3
-    binding.pry
+
   end
 
   def kind
