a1a4944ca70d9e4536f7246271d642ffa513f8ad
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-03 21:59:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,23 +9,23 @@ class Triangle
     @length_3 = length_3
   end
 
-  def trutriangle
-    @length_1 + @length_2 > @length_3 && (@length_1.positive? && @length_2.positive? && @length_3.positive?)
-  end
-
-  def kind
-    if trutriangle != Triangle
-        raise TriangleError
-    else
-      if @length_1 == (@length_2 && @length_3)
-        :equilateral.inspect
-      elsif @length_1 == @length_2 || @length_2 == @length_3 || @length_1 == @length_3
-        :isosceles.inspect
-      else
-        :scalene.inspect
-      end
-    end
-  end
+  # def trutriangle
+  #   @length_1 + @length_2 > @length_3 && (@length_1.positive? && @length_2.positive? && @length_3.positive?)
+  # end
+  #
+  # def kind
+  #   if trutriangle != Triangle
+  #       raise TriangleError
+  #   else
+  #     if @length_1 == (@length_2 && @length_3)
+  #       :equilateral.inspect
+  #     elsif @length_1 == @length_2 || @length_2 == @length_3 || @length_1 == @length_3
+  #       :isosceles.inspect
+  #     else
+  #       :scalene.inspect
+  #     end
+  #   end
+  # end
 
   class TriangleError < StandardError
   end
