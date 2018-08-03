dec371ff0eb7cd732d62844cf6fb932b7c2c3bd5
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-03 22:07:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @length_2 = length_2
     @length_3 = length_3
   end
-binding.pry
+#binding.pry
   # def trutriangle
   #   @length_1 + @length_2 > @length_3 && (@length_1.positive? && @length_2.positive? && @length_3.positive?)
   # end
