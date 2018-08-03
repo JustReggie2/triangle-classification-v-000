05b56ffa64f162bf37187c7f5b24390aa59f09ee
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-03 22:06:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Triangle
   attr_accessor :length_1, :length_2, :length_3
 
-  def inititalize(length_1, length_2, length_3)
+  def inititalize #(length_1, length_2, length_3)
     @length_1 = length_1
     @length_2 = length_2
     @length_3 = length_3
