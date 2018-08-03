8c23de864d6941e03d25710fdcb7b3b7022b0541
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-03 21:28:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3 
+
+  def inititalize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    trutriangle = @length_1 + @length_2 > @length_3 
+    if self.class != trutriangle
+        raise TriangleError
+    else
+      if @length_1 == (@length_2 && @length_3)
+        :equilateral.inspect
+      elsif @length_1 == @length_2 || @length_2 == @length_3 || @length_1 == @length_3
+        :isosceles.inspect
+      else
+        :scalene.inspect
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end 
+      
 end
